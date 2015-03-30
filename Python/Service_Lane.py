010b652e176bb2d98a05835d9ca9c8711975f642
NurbekSakiev <sakin.snn@gmail.com>
2015-03-29 23:01:22 -0500

    solution to Service Lane

---
+++ Python/Service_Lane.py
@@ -0,0 +1,23 @@
+## Version 1.0
+
+N,T = [int(x) for x in raw_input().split()]
+list = [int(x) for x in raw_input().split()]
+for l in xrange(T):
+    i,j = [int(y) for y in raw_input().split()]
+    smallest = 3
+    for k in xrange(i, j+1):
+        if(list[k]<smallest):
+            smallest = list[k]
+    print smallest
+
+## Version 2.0 (efficient)
+
+entry = raw_input().split()
+N = int(entry[0])
+T = int(entry[1])
+list = [int(x) for x in raw_input().split()]
+for x in xrange(T):
+    l = raw_input().split()
+    i = int(l[0])
+    j = int(l[1])
+    print min(list[i:j+1])

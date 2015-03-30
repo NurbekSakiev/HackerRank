a1f1e2ae21484107fafc3bcdf30aae23117b78c3
NurbekSakiev <sakin.snn@gmail.com>
2015-03-30 06:49:30 -0500

    solution to Angry_Professor

---
+++ Python/Angry_Professor.py
@@ -0,0 +1,16 @@
+T = int(raw_input())
+list = []
+for i in xrange(T):
+    entry = raw_input().split()
+    N = int(entry[0])
+    K = int(entry[1])
+    count = 0
+    list = [int(x) for x in raw_input().split()]
+    for j in xrange(N):
+        if(list[j]<=0):
+            count = count +1
+    if (count<K):
+        print "YES"
+    else:
+        print "NO"
+    

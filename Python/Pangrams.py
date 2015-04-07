16bcd5eadd2a766eab126fc555530d42f17753a7
NurbekSakiev <sakin.snn@gmail.com>
2015-04-07 00:33:24 -0500

    updated solution

--- Python/Pangrams.py
+++ Python/Pangrams.py
@@ -2,6 +2,8 @@
 # Pangrams
 # author: Nurbek Sakiev
 
+# Version 1.0
+
 from collections import Counter
 from collections import OrderedDict
 
@@ -13,6 +15,17 @@ my_list = list(a.keys())                                    ##take the keys of a
 my_list = map(lambda x: x.lower(),my_list)                  ##apply lower() to all items in a list
 my_list = ''.join(OrderedDict.fromkeys(sorted(my_list)))    ##join into ordered string the items(letters) in an array
 if alphabet == my_list:
+    print "pangram"
+else:
+    print "not pangram"
+
+# Version 2.0 (more efficient)
+
+
+# Create a full set with list (string) of all chars
+alphabet = set('abcdefghijklmnopqrstuvwxyz')
+sentence = raw_input()
+if not alphabet - set(sentence.lower()):
     print "pangram"
 else:
     print "not pangram"

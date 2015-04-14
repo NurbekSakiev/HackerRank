ad61505dcd7b2163a2d8bd2d824d427612a16df9
NurbekSakiev <sakin.snn@gmail.com>
2015-04-14 09:00:48 -0500

    final solution

--- Python/Cavity_Map.py
+++ Python/Cavity_Map.py
@@ -2,12 +2,17 @@
 # Cavity Map
 # author: Nurbek Sakiev
 
-n = int(raw_input())
-for i in xrange(n):
-    nums = raw_input()
-    nums = [int(i) for i in nums]
-    for j in xrange(1, len(nums)-1):
-        if(nums[j]>nums[j-1] and nums[j]>nums[j+1]):
-            nums[j] = "X"
-    print ''.join(map(str, nums))
-        
+import sys
+
+n = int(sys.stdin.readline())
+a = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j == n-1 or j ==0 or i == n-1:
+            print(a[i][j], end = '')
+        elif (a[i][j]>a[i-1][j] and a[i][j]>a[i][j-1] and a[i][j]>a[i][j+1] and a[i][j]>a[i+1][j]):
+            print('X',end= '')
+        else:
+            print(a[i][j],end = '') 
+    print()

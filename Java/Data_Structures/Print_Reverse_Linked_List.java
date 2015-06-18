94c572e32f71f69e1c4ea02b92bf36f3646a491d
NurbekSakiev <sakin.snn@gmail.com>
2015-06-18 16:33:29 -0500

    printing the reversed linked list

---
+++ Java/Data_Structures/Print_Reverse_Linked_List.java
@@ -0,0 +1,22 @@
+// Hacker Rank (www.hackerrank.com)
+// Print in Reverse
+// author: Nurbek Sakiev
+
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+void ReversePrint(Node head) {
+
+    if(head == null) {
+        return;
+    }
+    ReversePrint(head.next);
+    System.out.println(head.data);
+}

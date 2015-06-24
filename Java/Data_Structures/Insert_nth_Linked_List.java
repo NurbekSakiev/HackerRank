46750bb1eb146f4b3e6fd24d1279ead9f1124978
NurbekSakiev <sakin.snn@gmail.com>
2015-06-23 23:43:27 -0500

    update

--- Java/Data_Structures/Insert_nth_Linked_List.java
+++ Java/Data_Structures/Insert_nth_Linked_List.java
@@ -2,18 +2,6 @@
 // Insert a node at a specific position in a linked list
 // author: Nurbek Sakiev
 
-/*
-  Insert Node at the end of a linked list 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  class Node {
-     int data;
-     Node next;
-  }
-*/
-    // This is a "method-only" submission. 
-    // You only need to complete this method. 
-
 Node InsertNth(Node head, int data, int position) {
     Node newNode = new Node();
     newNode.data = data;

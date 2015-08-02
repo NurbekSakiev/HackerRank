277187bf571931b2891bb8002185fa18a1d58c21
NurbekSakiev <sakin.snn@gmail.com>
2015-08-02 06:02:49 -0500

    added version 2.0 of  Time Conversion

--- Python/Algorithms/Warm_Up/Time_Conversion.py
+++ Python/Algorithms/Warm_Up/Time_Conversion.py
@@ -2,6 +2,8 @@
 // Time_Conversion
 // author: Nurbek Sakiev
 
+# Version 1.0
+
 old = list(raw_input())
 if old[8] == 'A':
     if(''.join(old[:2]))=='12':
@@ -18,3 +20,18 @@ else:
         new = list(str(new))+old[2:8]
     new = ''.join(new)
 print new
+
+# Version 2.0
+
+t=raw_input()
+a=map(int,t[:-2].split(':'))
+t=t[-2:]
+if t=='PM' and 1<=a[0]<=11 :
+    a[0]+=12
+if t=='AM' and a[0]==12 :
+    a[0]-=12
+a=map(str,a)
+for i in range(3) :
+    if len(a[i])==1 :
+        a[i]='0'+a[i]
+print ':'.join(a)

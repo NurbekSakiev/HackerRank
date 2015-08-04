cfe069ac7e82efa3d738f820b53bccb2fbaa326a
NurbekSakiev <sakin.snn@gmail.com>
2015-08-03 22:04:23 -0500

    solution to Counting Sort 1

---
+++ Python/Algorithms/Sorting/Counting_Sort_1.py
@@ -0,0 +1,8 @@
+// Hacker Rank (www.hackerrank.com)
+// Counting_Sort_1
+// author: Nurbek Sakiev
+
+t = input()
+ar = [int(i) for i in raw_input().split()]
+for i in range(100):
+    print ar.count(i),

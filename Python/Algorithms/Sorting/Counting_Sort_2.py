b8c25d6c57f36903b21a3469f69a8025658d936a
NurbekSakiev <sakin.snn@gmail.com>
2015-08-04 08:33:27 -0500

    solution to Counting Sort 2

---
+++ Python/Algorithms/Sorting/Counting_Sort_2.py
@@ -0,0 +1,9 @@
+// Hacker Rank (www.hackerrank.com)
+// Counting_Sort_2
+// author: Nurbek Sakiev
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+t = input()
+ar = [int(i) for i in raw_input().split()]
+for i in sorted(ar):
+	print i,
